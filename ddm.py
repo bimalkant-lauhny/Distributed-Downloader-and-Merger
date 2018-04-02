9cc823832aa53e0795982b14572bffe059c831cd
Bimalkant Lauhny <lauhny.bimalk@gmail.com>
2018-04-02 05:54:37 +0530

    Separate ArgsHandler Class

--- ddm.py
+++ ddm.py
@@ -3,81 +3,18 @@ import logging
 import os
 import sys
 import shutil
-import argparse
 import threading
 import pathlib
 import socket
+from argshandler import ArgsHandler
 
-class DistributedDownloaderAndMerger:
+class DistributedDownloaderAndMerger(ArgsHandler):
 
 	''' Main class providing interface of the software'''
 
 	def __init__(self):
-		self.args = None
+		ArgsHandler.__init__(self)
 		logging.getLogger("urllib3").setLevel(logging.WARNING)
-
-	# handles command line arguments
-	def handle_args(self):
-
-		parser = argparse.ArgumentParser()
-
-		parser.add_argument("--url", 
-			help="Enter the URL of the file (Required)")
-
-		parser.add_argument("--proxy", 
-			help='''Enter the proxy if required, 
-			default = no proxy''')
-
-		parser.add_argument("--dir", 
-			help='''Enter the directory to store the downloaded file, 
-			default = ~/Downloads/''')
-
-		parser.add_argument("--tempdir", 
-			help='''Enter the directory to store the temporary files, 
-			default = ~/Downloads/.ddmtemp/''')
-
-		parser.add_argument("--timeout", 
-			help='''Enter the request timeout in seconds,
-			default = 5.0''', 
-			type=float)
-
-		parser.add_argument("--retries", 
-			help='''Enter the number of retries,
-			default = 5''',
-			type=int)
-
-		parser.add_argument("--threads", 
-			help='''Enter the number of threads,
-			default = 2''',
-			type=int)
-
-		self.args = parser.parse_args()
-
-		self.handle_default_args()
-
-	# sets default arguments if not supplied by the user
-	def handle_default_args(self):
-
-		if not self.args.url:
-			print("You didn't supply the download URL! See --help or -h for details.")	
-			sys.exit()
-
-		if not self.args.dir:
-			self.args.dir = str(pathlib.Path.home()) + "/Downloads/"
-
-		if not self.args.tempdir:
-			self.args.tempdir = self.args.dir + "/.ddmtemp/" 
-			# create temp directory
-			self.create_dir(self.args.tempdir)
-
-		if not self.args.timeout:
-			self.args.timeout = 5.0	
-
-		if not self.args.retries:
-			self.args.retries = 5
-
-		if not self.args.threads:
-			self.args.threads = 2
 
 	# function for clean deletion of a file at filepath
 	def delete_file(self, filepath):
@@ -128,11 +65,11 @@ class DistributedDownloaderAndMerger:
 		return resp
 
 	# function which acts as handler for download threads
-	def seg_handler(self, filepath, range_left, range_right):
+	def seg_handler(self, tempfilepath, range_left, range_right):
 		resp = self.make_request({'Range': 'bytes=%d-%d' % (range_left, range_right)})
 		chunk_size = 1024 * 256 #256KB
 
-		with open(filepath, "wb") as fp:
+		with open(tempfilepath, "wb") as fp:
 			downloaded = 0 #in KBs
 			while True:
 				data = resp.read(chunk_size)
@@ -198,7 +135,7 @@ class DistributedDownloaderAndMerger:
 			# calling seg_handler() for each thread
 			t = threading.Thread(target=self.seg_handler,
 				kwargs={
-				'filepath': self.args.tempdir + "/temp" + str(f), 
+				'tempfilepath': self.args.tempdir + "/temp" + str(f), 
 				'range_left': ranges_list[f][0],
 				'range_right': ranges_list[f][1]
 				})
